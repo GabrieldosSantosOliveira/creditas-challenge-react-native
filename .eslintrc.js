af7a3b44d57b72dae0a0deac4a4765468509351f
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2024-05-25 11:53:17 -0400

    chore: add setup for jest and @testing-library/react-native

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,15 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
+  overrides: [
+    {
+      // Test files only
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["plugin:testing-library/react"],
+    },
+  ],
+  env: {
+    jest: true,
+  },
   extends: ["expo", "prettier"],
   plugins: ["prettier"],
   rules: {
